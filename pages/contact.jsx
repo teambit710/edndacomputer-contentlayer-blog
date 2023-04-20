748b8544167ef9c6792e43b85a987ef90c9f7b8d
ed <edndacomputer@riseup.net>
2023-04-19 20:20:14 -0700

    google fonts

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -2,16 +2,19 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import Button from "../components/layout/Button";
+import { PhoneCall, MapPin, EnvelopeOpen } from "@phosphor-icons/react";
+
 export const Form = () => {
   const schema = yup.object().shape({
     fullName: yup.string().required("Your Full Name is Required!"),
-    email: yup.string().email().required(),
-    age: yup.number().positive().integer().min(18).required(),
-    password: yup.string().min(4).max(20).required(),
-    confirmPassword: yup
-      .string()
-      .oneOf([yup.ref("password"), null], "Passwords Don't Match")
-      .required(),
+    email: yup.string().email().required("Enter a valid email address!"),
+    age: yup
+      .number()
+      .positive()
+      .integer()
+      .min(18)
+      .required("Enter a valid age!"),
+    textarea: yup.string().required("Your Message is Required!"),
   });
 
   const {
@@ -27,65 +30,40 @@ export const Form = () => {
   };
 
   return (
-    <section className="py-6 bg-white text-gray-500">
+    <section className="py-6 bg-white text-gray-900">
       <div className="grid max-w-6xl grid-cols-1 px-6 mx-auto lg:px-8 md:grid-cols-2 md:divide-x">
-        <div className="py-6 md:py-0 md:px-6">
+        <div className="rounded-lg shadow-lg py-6 md:py-0 md:px-6 bg-gray-300 p-4 flex-col flex items-center justify-center">
           <h1 className="text-4xl font-bold">Get in touch</h1>
           <p className="pt-2 pb-4">Fill in the form to start a conversation</p>
           <div className="space-y-4">
             <p className="flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="w-5 h-5 mr-2 sm:mr-6"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
+              <MapPin size={48} color="#ff66b3" weight="duotone" />
               <span>Fake address, 9999 City</span>
             </p>
             <p className="flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="w-5 h-5 mr-2 sm:mr-6"
-              >
-                <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
-              </svg>
+              <PhoneCall size={48} color="#ff66b3" weight="duotone" />
               <span>123456789</span>
             </p>
             <p className="flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="w-5 h-5 mr-2 sm:mr-6"
-              >
-                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
-                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
-              </svg>
+              <EnvelopeOpen size={48} color="#ff66b3" weight="duotone" />
               <span>contact@business.com</span>
             </p>
           </div>
         </div>
         <form
+          onSubmit={handleSubmit(onSubmit)}
           novalidate=""
           className="flex flex-col py-6 space-y-6 md:py-0 md:px-6 ng-untouched ng-pristine ng-valid"
         >
           <label className="block">
             <span className="mb-1">Full name</span>
             <input
-              className="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-green-400 bg-primary-light"
+              className="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-primary-dark bg-gray-100"
               type="text"
               placeholder="Full Name..."
               {...register("fullName")}
-            /> 
-            <p>{errors.fullName?.message}</p>
+            />
+            <p className="text-sm text-red-500">{errors.fullName?.message}</p>
           </label>
           <label className="block">
             <span className="mb-1">Email address</span>
@@ -93,14 +71,16 @@ export const Form = () => {
               type="text"
               placeholder="Email..."
               {...register("email")}
-              className="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-green-400 bg-primary-light"
+              className="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-primary-dark bg-gray-100"
             />
+            <p className="text-sm text-red-500">{errors.email?.message}</p>
           </label>
           <label className="block">
             <span className="mb-1">Message</span>
             <textarea
               rows="3"
-              className="block w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-green-400 bg-primary-light"
+              {...register("textarea")}
+              className="block w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-primary-dark bg-gray-100"
             ></textarea>
           </label>
           <Button className="self-center" color="primary">
