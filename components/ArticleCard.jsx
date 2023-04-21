c6ca309ce34c530561750fdf4300cc383b5bd912
ed <edndacomputer@riseup.net>
2023-04-21 01:49:26 -0700

    blog list cards

--- components/ArticleCard.jsx
+++ components/ArticleCard.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { ArrowRight, Calendar, Books } from "@phosphor-icons/react";
+import avatar from "../public/images/avatar.jpg";
+
+import { ArrowRight, Timer, Calendar } from "@phosphor-icons/react";
 
 export default function ArticleCard({
   title,
@@ -13,58 +15,61 @@ export default function ArticleCard({
   readingTime,
 }) {
   return (
-    <section className="py-6">
-      <div className="dark:bg-gray-100 dark:text-gray-900 flex flex-row rounded-lg overflow-hidden shadow-lg">
-        <Image
-          className="lg:h-48 md:h-36 w-full object-cover object-center hidden md:block"
-          src={image}
-          width={720}
-          height={400}
-          alt="blog"
-        />
-        <div className="flex flex-col p-6 col-span-full row-span-full lg:col-span-8 lg:p-4">
-          <div className="flex justify-start">
-            <span className="bg-blue-100 text-primary-dark text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-primary dark:text-gray-50">
+    <div className="flex flex-col justify-center">
+      <div className="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-lg shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-gray-100">
+        <div className="w-full md:w-1/3 bg-white grid place-items-center">
+          <img src={image} alt="tailwindlogo" className="rounded-xl" />
+        </div>
+        <div className="w-full md:w-2/3 bg-white flex flex-col space-y-2 p-3">
+          <div className="flex justify-between item-center">
+            <p className="text-gray-500 font-medium hidden md:block">
+              {readingTime}
+            </p>
+            <div className="flex items-center">
+              <Timer size={12} color="#ff66b3" weight="duotone" />
+              <p className="text-gray-600 font-bold text-sm ml-1">
+                <span className="text-gray-500 font-normal">{dateTime}</span>
+              </p>
+            </div>
+            <div className="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 text-pink-500"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+
+            <span class="bg-primary-light text-gray-100 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-primary-dark outline-2">
               {category}
             </span>
           </div>
-          <h1 className="md:text-3xl text-xl font-semibold"> {title}</h1>
-          <p className="flex-1 pt-2">{description}</p>
-          <Link rel="noopener noreferrer" href={`/article/${slug}`}>
-            <div className="inline-flex items-center space-x-1">
-              <span className="text-xl text-primary-light">Read more</span>
-              <ArrowRight size={48} color="#ff66b3" weight="duotone" />
-            </div>
-          </Link>
-          <div className="flex items-center justify-between pt-2">
-            <div className="flex space-x-2">
-              <div className="relative flex h-10 w-10 shrink-0 select-none items-center justify-center rounded-full bg-transparent text-sm font-bold uppercase text-gray-800">
-                <img
-                  className="h-full w-full rounded-full object-cover object-center ring-2 ring-transparent dark:ring-primary-light p-0.5"
-                  src={"/images/avatar.jpg"}
-                  alt="avatar"
-                />
-              </div>
-              <div className="flex flex-col pl-2">
-                <span className="self-center text-sm">by edndacomputer</span>
-                <span className="self-center text-sm inline-flex space-x-2">
-                  <Calendar size={32} color="#ff66b3" weight="duotone" />
-                  <div>{dateTime}</div>
-                </span>
-              </div>
-            </div>
-            <span className="text-xs">
-              <Books
+          <h3 className="font-black text-gray-800 md:text-3xl text-xl">
+            {title}
+          </h3>
+          <p className="md:text-lg text-gray-500 text-base">{description}</p>
+          <p className="text-lg font-black text-gray-800">
+            <a
+              href={`/article/${slug}`}
+              className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0"
+            >
+              Read More
+              <ArrowRight
                 size={48}
                 color="#ff66b3"
                 weight="duotone"
-                className="hidden md:block"
+                className="h-6 w-6"
               />
-              <span className="text-xs">{readingTime}</span>
-            </span>
-          </div>
+            </a>
+          </p>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,5 +21,6 @@ module.exports = {
   plugins: [
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
   ],
 };
